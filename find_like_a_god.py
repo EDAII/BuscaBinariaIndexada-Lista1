8dabcbe270db1ddd7fad50819b896c4b212e65b1
victorhdcoelho <victorhdcoelho@gmail.com>
2018-03-23 20:44:47 -0300

    Add and remove working

--- find_like_a_god.py
+++ find_like_a_god.py
@@ -3,20 +3,8 @@ from random import randint
 import os
 import time
 import timeit
-max_len = 100
 
-def mix_search(list_db,num_search):
-    tic = timeit.default_timer()
-    insertion_sort(list_db)
-    toc = timeit.default_timer()
-    print("-------------------------------------------------")
-    print("| Time insertion_sort: {}    |".format(toc-tic))
-    print("-------------------------------------------------")
-    tic = timeit.default_timer()
-    list_index = index_list(list_db)
-    toc = timeit.default_timer()
-    print("| Time index_list: {}        |".format(toc-tic))
-    print("----------------------------------------------------------")
+def mix_search(list_db,num_search,list_index):
     tic = timeit.default_timer()
     end , begin = sequencial_search(num_search,list_index)
     toc = timeit.default_timer()
@@ -45,12 +33,16 @@ def insertion_sort(list_db = []):
             next_item-=1
 
 def index_list(list_db = []):
-    list_index=[]
-    index = [index-1 for index in range(0,max_len,10)]
-    index.append(max_len-1)
+    index = [index-1 for index in range(0,len(list_db),30)]
+    #print(index)
+    index.append(len(list_db)-1)
     index[0]+=1
-    list_values = [list_db[code] for code in index]
-    list_values = list(zip(list_values,index))
+    #print(index)
+    list_values = []
+    for code in index:
+        #print(code)
+        list_values.append([list_db[code],code])
+    #print(list_values)
     return list_values
 
 def sequencial_search(num_search,list_num):
@@ -63,7 +55,7 @@ def sequencial_search(num_search,list_num):
         count+=1
     return -1,-1
 
-def search_binary(list_db,number,begin = 0,end = max_len -1):
+def search_binary(list_db,number,begin,end):
     while begin <= end :
         find = int((begin + end) / 2)
         if list_db[find] == number:
@@ -74,46 +66,62 @@ def search_binary(list_db,number,begin = 0,end = max_len -1):
             end = find - 1
     return -1
 
-def init_list(db):
-    list_db = []
-    for line in db:
-        list_db.append(int(line))
-    db.close()
-    return list_db
+def insert_list(ans_1,list_db):
+    list_db.append(ans_1)
 
-
-def choise(answer,list_db):
+def choise(answer,list_db,list_index):
     if answer == 0:
         print("Good bye!!")
 
     if answer == 1:
-        print("Unsorted list :\n {}".format(list_db))
-
-    if answer == 2:
-        insertion_sort(list_db)
         print("Sorted list :\n {}".format(list_db))
-
+    #################################################
+    if answer == 2:
+        flag=False
+        print("Write a number of your choise!\n")
+        ans_1 = int(input())
+        tic = timeit.default_timer()
+        for each in list_db:
+            if each == ans_1:
+                flag=True
+                print("Found: {}".format(each))
+                break;
+        toc = timeit.default_timer()
+        print("Time: {}".format(toc-tic))
+        if not flag:
+            print("Don't find anything\n")
+    ###############################################
     if answer == 3:
         print("Write a number of your choise!\n")
         ans_1 = int(input())
         tic = timeit.default_timer()
-        ans_2 = search_binary(list_db,ans_1)
+
+        ans_2 = mix_search(list_db,ans_1,list_index)
+
         toc = timeit.default_timer()
         if ans_2 != -1:
             print("Found: {} in time: {}".format(list_db[ans_2],toc-tic))
 
         else:
             print("Don't find anything\n")
-
+    ##############################################
     if answer == 4:
+        print("What number you want to insert: ")
+        ans_1 = int(input())
+        insert_list(ans_1,list_db)
+        insertion_sort(list_db)
+        list_index = index_list(list_db)
+    ##############################################
+    if answer == 5:
         print("Write a number of your choise!\n")
         ans_1 = int(input())
         tic = timeit.default_timer()
-        ans_2 = mix_search(list_db,ans_1)
+        ans_2 = mix_search(list_db,ans_1,list_index)
         toc = timeit.default_timer()
         if ans_2 != -1:
-            print("Found: {} in time: {}".format(list_db[ans_2],toc-tic))
-
+            print("Delete: {} in time: {}".format(list_db[ans_2],toc-tic))
+            list_db.pop(ans_2)
+            insertion_sort(list_db)
         else:
             print("Don't find anything\n")
 
@@ -121,29 +129,25 @@ def choise(answer,list_db):
 
 
 if __name__ == "__main__":
-
+    list_db=[]
+    list_index=[]
+    print("Write the len, you want, of your list:")
+    max_len=int(input())
     path = os.path.dirname(os.path.abspath(__file__))
-    new_path = os.path.join(path,"db.txt")
-    try:
-        db = open(new_path,"r")
-    except:
-        db = open("db.txt","w+")
-        for count in range(0,max_len):
-            number = randint(0,99999)
-            db.write("%d \n" %(number))
-        db.close()
-    db = open(new_path,"r")
-    list_db = init_list(db)
+    for count in range(0,max_len):
+        list_db.append(randint(0,99999))
+    insertion_sort(list_db)
+    list_index = index_list(list_db)
     answer = 10
     while answer > 0:
-        print("Choose one of choises bellow:\n1-Print unorder list.\n2-Print order list.\n3-Binary search.\n4-Mix search\n0-Getout here.")
+        print("Choose one of choises bellow:\n1-Print order list.\n2-sequencial search.\n3-Mix search\n4-Insert item\n5-Delete item\n0-Getout here.")
         answer = int(input())
-        choise(answer,list_db)
-        print("Press enter to continue!!!")        
+        choise(answer,list_db,list_index)
+        list_index = index_list(list_db)
+        print("Press enter to continue!!!")
         try:
             input()
         except:
             print("You are in python2 please press enter again !!!")
             raw_input()
-        #time.sleep(3)
         os.system("clear")
